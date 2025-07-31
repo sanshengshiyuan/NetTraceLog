ca0d721ffb8bba7a79f0e0e6d281822f21caa08f
zhoufeifan <1342120146@qq.com>
2025-07-31 22:55:36 +0800

    fix(defer):将defer模块代码补充完整了

---
+++ logger/defer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <functional>
+
+namespace logger {
+class ExecuteOnScopeExit {
+public:
+    ExecuteOnScopeExit() = default;
+
+    ExecuteOnScopeExit(ExecuteOnScopeExit&& ) = default;
+
+    ExecuteOnScopeExit& operator = (ExecuteOnScopeExit&& ) = default;
+
+    ExecuteOnScopeExit(const ExecuteOnScopeExit& ) = delete;
+
+    ExecuteOnScopeExit& operator = (const ExecuteOnScopeExit& ) = delete;
+
+    template <typename F, typename... Args> 
+    ExecuteOnScopeExit(F&& f, Args&&... args) {
+        func_ = std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    }
+
+    ~ExecuteOnScopeExit() {
+        if (func_) {
+            func_();
+        }
+    }
+private:
+    std::function<void()> func_;
+    
+};
+}
+
+#define DEFER(lambda) logger::ExecuteOnScopeExit CONCAT(defer_, __LINE__) = lambda
